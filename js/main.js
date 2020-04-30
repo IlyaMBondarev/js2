d826f76b32bbd666e3b3572cf0a3da4bb0bddbaf
ilyaBondarev <12ilya92@mail.ru>
2020-04-30 15:45:00 +0300

    3 дз

--- js/main.js
+++ js/main.js
@@ -1,27 +1,162 @@
-const products = [
-  {id: 1, title: 'Notebook', price: 20000},
-  {id: 2, title: 'Mouse', price: 1500},
-  {id: 3, title: 'Keyboard', price: 5000},
-  {id: 4, title: 'Gamepad', price: 4500},
-];
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-const renderProduct = (title, price, presence = "В наличии") => {
-  return `<div class="product-item">
-            <h2>${title}</h2>
-            <p>Цена: ${price} руб.</p>
-            <p>${presence}</p>
-            <button class="buy-btn">Добавить в корзину</button>
-          </div>`;
+let getRequest = (url, file) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (url) {
+                resolve(url + file);
+            } else {
+                reject('Error!');
+            }
+        }, 100);
+    });
 };
 
-const renderProducts = (list) => { //Запятая выводилась, так как innerHTML выводит абсолютно весь текст, включая запятую между элементами массива
-  list.forEach(good => document.querySelector('.products').insertAdjacentHTML('beforeend', renderProduct(good.title, good.price)))
-};
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this._getProducts()
+            .then(data => {
+                this.goods = [...data];
+                this._render();
+                new CartList();
+            });
+    }
 
-let btnCart = document.querySelector(".btn-cart");
+    _getProducts() {
+        return fetch(`${API}/catalogData.json`)
+            .then(response => response.json())
+            .catch(error => {
+                console.log(error);
+            });
+    }
 
-btnCart.addEventListener('click', function () {
-  alert("Пока ничего в корзину не добавить, ведутся технические работы")
-});
+    calcSum() {
+        return this.goods.reduce((sum, good) => sum + good.price, 0);
+    }
 
-renderProducts(products);
+    _render() {
+        const block = document.querySelector(this.container);
+
+        for (let product of this.goods) {
+            const productObject = new ProductItem(product);
+            this.allProducts.push(productObject);
+            block.insertAdjacentHTML('beforeend', productObject.render());
+        }
+    }
+}
+
+class ProductItem {
+    constructor(product, img = 'https://placehold.it/200x150') {
+        this.title = product.product_name;
+        this.price = product.price;
+        this.id = product.id_product;
+        this.img = img;
+    }
+
+    render() {
+        return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <div class="desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} \u20bd</p>
+                    <button class="buy-btn" data-id="${this.id}" data-title="${this.title}" data-price="${this.price}" data-img="${this.img}">Купить</button>
+                </div>
+            </div>`;
+    }
+}
+
+function showCartList() {
+    document.querySelector('.cart').classList.toggle('hidden');
+}
+
+class CartList {
+    constructor() {
+        this.container = '.cart';
+        this.cartProducts = [];
+        this.sum = 0;
+        this.count = 0;
+        this.allCartProducts = [];
+        this._getBasket()
+            .then(data => {
+                this.cartProducts = [...data.contents];
+                this._render();
+            });
+        document.querySelectorAll('.buy-btn').forEach(function (buyBtn) {
+            buyBtn.addEventListener('click', event => CartList.addProduct(event))
+        });
+        document.querySelector('.btn-cart').addEventListener('click', event => showCartList());
+    }
+
+    _getBasket() {
+        return fetch(`${API}/getBasket.json`)
+            .then(response => response.json())
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    _render() {
+        const block = document.querySelector(this.container);
+
+        for (let product of this.cartProducts) {
+            const productObject = new CartItem(product);
+            this.allCartProducts.push(productObject);
+            block.insertAdjacentHTML('beforeend', productObject.render());
+            this.sum += product.price;
+            this.count += product.quantity;
+        }
+        block.insertAdjacentHTML('beforeend', `<p class="total">Общая сумма: ${this.sum}</p>
+                                               <p class="total">Количество предметов: ${this.count}</p>`)
+    }
+
+    static addProduct(buyBtn) {
+        return fetch(`${API}/addToBasket.json`)
+            .then(response => response.json())
+            .then(data => {
+                if (data.result) {
+                    if (!this.checkIfAdded(buyBtn)) {
+                        this.allCartProducts.push(new CartItem(cartItem));
+                        this._render();
+                        this.sum += buyBtn.dataset.price;
+                        this.count++;
+                    }
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    checkIfAdded(cartItem) {
+        for (const productObject in this.allCartProducts) {
+            if (cartItem.dataset === productObject.id_product) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+class CartItem {
+    constructor(product, img = 'https://placehold.it/200x150') {
+        this.title = product.product_name;
+        this.price = product.price;
+        this.id = product.id_product;
+        this.img = img;
+    }
+
+    render() {
+        return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <div class="desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} \u20bd</p>
+                    </div>
+            </div>`;
+    }
+}
+
+new ProductList();
